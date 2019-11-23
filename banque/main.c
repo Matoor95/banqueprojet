6f140fadc389bd84f34046ccc59df608f45e71fe
Matar95 <matseckserigne95@gmail.com>
2019-11-23 01:21:57 +0100

    Initial Commit

---
+++ banque/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  banque
+//
+//  Created by matarseck on 23/11/2019.
+//  Copyright © 2019 matarseck. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
